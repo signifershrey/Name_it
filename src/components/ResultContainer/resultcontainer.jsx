5c170b2ca442966777b8cceb7384440cd92f4ea5
SHREY MAURYA <56041735+signifershrey@users.noreply.github.com>
2021-02-01 14:11:01 +0530

    Update Project

--- src/components/ResultContainer/resultcontainer.jsx
+++ src/components/ResultContainer/resultcontainer.jsx
@@ -2,20 +2,21 @@ import React from 'react'
 import './resultcontainer.css'
 import NameCard from '../Namecard/namecard'
 
+
 const ResultsContainer = ({ suggestedNames }) => {
 
-    const suggestedNameJsx = suggestedNames.map(suggestedName => {
+    const suggestedNameJsx = suggestedNames.map((suggestedName) => {
         // return <p key={suggestedName}>{suggestedName}</p>
         return <NameCard key={suggestedName} suggestedName={suggestedName} />
-    })
+    });
+
 
 
     return (
         <div className="result-container">
-            <p>These are results!</p>
-            <p>{/*suggestedNames.length > 0 ? suggestedNames[0] : null*/} </p>
-            <p>{suggestedNameJsx}</p>
-            <p>{/*suggestedNames.length > 0 && suggestedNames[0]*/}</p>
+
+            {suggestedNameJsx}
+
         </div>
     )
 }
